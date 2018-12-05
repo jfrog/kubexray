2ad0aa767ddf3e971c7256ee0a423aff8df5d061
travisf <travisf@jfrog.com>
2018-12-05 14:27:32 -0800

    Reinstated issue check
    Reverted change to check violations instead of issues (the violations
    code is now commented out). This is because the violations check depends
    on an unreleased xray API, and won't work immediately. Once the API is
    released, this should be changed back.

--- cmd/kubexray/handler.go
+++ cmd/kubexray/handler.go
@@ -634,88 +634,138 @@ type ViolationAPIResponse struct {
 	Data  []ViolationAPIResponseItem `json:"data"`
 }
 
+// func checkXray(sha2, url, user, pass string) (bool, bool, bool, error) {
+// 	log.Debugf("Checking sha %s with Xray ...", sha2)
+// 	var data ComponentAPIResponse
+// 	err := func(data *ComponentAPIResponse) error {
+// 		client := &http.Client{}
+// 		req, err := http.NewRequest("GET", url+"/api/v1/componentIdsByChecksum/"+sha2, nil)
+// 		if err != nil {
+// 			log.Warnf("Error checking xray: %s", err)
+// 			return err
+// 		}
+// 		req.SetBasicAuth(user, pass)
+// 		resp, err := client.Do(req)
+// 		if err != nil {
+// 			log.Warnf("Error checking xray: %s", err)
+// 			return err
+// 		}
+// 		defer resp.Body.Close()
+// 		if resp.StatusCode != 200 {
+// 			log.Warnf("Error checking xray: response code is %s", resp.Status)
+// 			return errors.New("xray server responded with status: " + resp.Status)
+// 		}
+// 		err = json.NewDecoder(resp.Body).Decode(data)
+// 		if err != nil {
+// 			log.Warnf("Error checking xray: %s", err)
+// 			return err
+// 		}
+// 		return nil
+// 	}(&data)
+// 	if err != nil {
+// 		return false, false, false, err
+// 	}
+// 	if len(data.Components) <= 0 {
+// 		log.Debug("Xray does not recognize this sha")
+// 		return false, false, false, nil
+// 	}
+// 	for _, comp := range data.Components {
+// 		bodyjson, err := json.Marshal(&comp)
+// 		if err != nil {
+// 			log.Warnf("Error checking xray: %s", err)
+// 			return false, false, false, err
+// 		}
+// 		var resp ViolationAPIResponse
+// 		err = func(data *ViolationAPIResponse) error {
+// 			client := &http.Client{}
+// 			path := "/ui/userIssues/details?direction=asc&order_by=severity&num_of_rows=0&page_num=0"
+// 			body := bytes.NewReader(bodyjson)
+// 			req, err := http.NewRequest("POST", url+path, body)
+// 			if err != nil {
+// 				log.Warnf("Error checking xray: %s", err)
+// 				return err
+// 			}
+// 			req.SetBasicAuth(user, pass)
+// 			req.Header.Add("Content-Type", "application/json")
+// 			resp, err := client.Do(req)
+// 			if err != nil {
+// 				log.Warnf("Error checking xray: %s", err)
+// 				return err
+// 			}
+// 			defer resp.Body.Close()
+// 			if resp.StatusCode != 200 {
+// 				log.Warnf("Error checking xray: response code is %s", resp.Status)
+// 				return errors.New("xray server responded with status: " + resp.Status)
+// 			}
+// 			err = json.NewDecoder(resp.Body).Decode(data)
+// 			if err != nil {
+// 				log.Warnf("Error checking xray: %s", err)
+// 				return err
+// 			}
+// 			return nil
+// 		}(&resp)
+// 		if err != nil {
+// 			return false, false, false, err
+// 		}
+// 		for _, item := range resp.Data {
+// 			if item.Severity == "High" {
+// 				if item.Type == "security" {
+// 					log.Infof("Major security violation found for sha: %s", sha2)
+// 					return true, true, false, nil
+// 				} else if item.Type == "licenses" || item.Type == "license" {
+// 					log.Infof("Major license violation found for sha: %s", sha2)
+// 					return true, false, true, nil
+// 				}
+// 			}
+// 		}
+// 	}
+// 	log.Debug("No major security issues found")
+// 	return true, false, false, nil
+// }
+
 func checkXray(sha2, url, user, pass string) (bool, bool, bool, error) {
 	log.Debugf("Checking sha %s with Xray ...", sha2)
-	var data ComponentAPIResponse
-	err := func(data *ComponentAPIResponse) error {
-		client := &http.Client{}
-		req, err := http.NewRequest("GET", url+"/api/v1/componentIdsByChecksum/"+sha2, nil)
-		if err != nil {
-			log.Warnf("Error checking xray: %s", err)
-			return err
-		}
-		req.SetBasicAuth(user, pass)
-		resp, err := client.Do(req)
-		if err != nil {
-			log.Warnf("Error checking xray: %s", err)
-			return err
-		}
-		defer resp.Body.Close()
-		if resp.StatusCode != 200 {
-			log.Warnf("Error checking xray: response code is %s", resp.Status)
-			return errors.New("xray server responded with status: " + resp.Status)
-		}
-		err = json.NewDecoder(resp.Body).Decode(data)
-		if err != nil {
-			log.Warnf("Error checking xray: %s", err)
-			return err
-		}
-		return nil
-	}(&data)
+	client := &http.Client{}
+	body := strings.NewReader("{\"checksums\":[\"" + sha2 + "\"]}")
+	req, err := http.NewRequest("POST", url+"/api/v1/summary/artifact", body)
 	if err != nil {
+		log.Warnf("Error checking xray: %s", err)
 		return false, false, false, err
 	}
-	if len(data.Components) <= 0 {
+	req.SetBasicAuth(user, pass)
+	req.Header.Add("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Warnf("Error checking xray: %s", err)
+		return false, false, false, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		log.Warnf("Error checking xray: response code is %s", resp.Status)
+		return false, false, false, errors.New("xray server responded with status: " + resp.Status)
+	}
+	var data interface{}
+	json.NewDecoder(resp.Body).Decode(&data)
+	dt := data.(map[string]interface{})
+	artifacts := dt["artifacts"].([]interface{})
+	if len(artifacts) <= 0 {
 		log.Debug("Xray does not recognize this sha")
 		return false, false, false, nil
 	}
-	for _, comp := range data.Components {
-		bodyjson, err := json.Marshal(&comp)
-		if err != nil {
-			log.Warnf("Error checking xray: %s", err)
-			return false, false, false, err
-		}
-		var resp ViolationAPIResponse
-		err = func(data *ViolationAPIResponse) error {
-			client := &http.Client{}
-			path := "/ui/userIssues/details?direction=asc&order_by=severity&num_of_rows=0&page_num=0"
-			body := bytes.NewReader(bodyjson)
-			req, err := http.NewRequest("POST", url+path, body)
-			if err != nil {
-				log.Warnf("Error checking xray: %s", err)
-				return err
+	for _, artifact := range artifacts {
+		art := artifact.(map[string]interface{})
+		issues := art["issues"].([]interface{})
+		for _, issue := range issues {
+			is := issue.(map[string]interface{})
+			typ := is["issue_type"].(string)
+			sev := is["severity"].(string)
+			if typ == "security" && sev == "Major" {
+				log.Infof("Major security issue found for sha: %s", sha2)
+				return true, true, false, nil
 			}
-			req.SetBasicAuth(user, pass)
-			req.Header.Add("Content-Type", "application/json")
-			resp, err := client.Do(req)
-			if err != nil {
-				log.Warnf("Error checking xray: %s", err)
-				return err
-			}
-			defer resp.Body.Close()
-			if resp.StatusCode != 200 {
-				log.Warnf("Error checking xray: response code is %s", resp.Status)
-				return errors.New("xray server responded with status: " + resp.Status)
-			}
-			err = json.NewDecoder(resp.Body).Decode(data)
-			if err != nil {
-				log.Warnf("Error checking xray: %s", err)
-				return err
-			}
-			return nil
-		}(&resp)
-		if err != nil {
-			return false, false, false, err
-		}
-		for _, item := range resp.Data {
-			if item.Severity == "High" {
-				if item.Type == "security" {
-					log.Infof("Major security violation found for sha: %s", sha2)
-					return true, true, false, nil
-				} else if item.Type == "licenses" || item.Type == "license" {
-					log.Infof("Major license violation found for sha: %s", sha2)
-					return true, false, true, nil
-				}
+			if typ == "license" && sev == "Major" {
+				log.Infof("Major license issue found for sha: %s", sha2)
+				return true, false, true, nil
 			}
 		}
 	}
