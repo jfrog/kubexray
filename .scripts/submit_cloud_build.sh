a47d2f826225918ee3bfbb2389da775e4ce49c7d
rimas <rmocius@gmail.com>
2019-08-27 14:27:43 +0100

    Add JFrog Pipelines integration

--- .scripts/submit_cloud_build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-GEN_COMMIT_SHA=$(openssl rand -base64 32 | tr -dc A-Za-z0-9 | head -c 12)
-
-source SUBSTITUTIONS
-
-gcloud builds submit . --config=.pipeline/cloudbuild-ci.yaml \
-    --substitutions=COMMIT_SHA="${GEN_COMMIT_SHA}",_GCS_BUCKET="${GCS_BUCKET}",_ZONE="${ZONE}",_CLUSTER_NAME="${CLUSTER_NAME}",_ARTIFACTORY="${ARTIFACTORY}" --project="${GCP_PROJECT}"
--- Makefile
+++ Makefile
@@ -13,13 +13,3 @@ build:
 	@echo "++ Building kubexray go binary..."
 	mkdir -p bin
 	cd cmd/kubexray && go build -a --installsuffix cgo --ldflags="-s" -o ../../bin/kubexray
-
-.PHONY: cloud
-cloud:
-	@echo "++ Submiting CI cloud build..."
-	.scripts/submit_cloud_build.sh
-
-.PHONY: creds
-creds:
-	@echo "++ Uploading creds files to GCS bucket..."
-	.scripts/upload_creds.sh
